82cd05765519b228e05cb5734c63b72c942daba0
golanruang <guang.rolan@gmail.com>
2024-01-12 22:32:02 -0800

    FAT COMMIT

--- sbhacks2024/App.js
+++ sbhacks2024/App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppProvider } from '../sbhacks2024/AppContext'
+
 import HomeScreen from '../sbhacks2024/screens/home';
+import LoadScreen from '../sbhacks2024/screens/load'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <HomeScreen />
-      <StatusBar style="auto" />
-    </View>
+    <AppProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Load" component={LoadScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppProvider>
   );
 }
 
