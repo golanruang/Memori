f3956d775abb208d1ecc14a00e8ae38efcce4625
Lindsey Wen <lindseywen@ucsb.edu>
2024-01-14 01:18:38 -0800

    welcome, signin, signup page

--- sbhacks2024/screens/welcome.js
+++ sbhacks2024/screens/welcome.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import { Text, Pressable, Image, View, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react";
+import {
+  Text,
+  Pressable,
+  Image,
+  View,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 
 function WelcomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <LinearGradient colors={['#141e30', '#243b55']} style={styles.linearGradient}>
+      <ImageBackground
+        source={require("sbhacks2024/assets/homebackground.jpeg")} // Replace with your image path
+        resizeMode="cover" // or "contain", depending on your need
+        style={styles.imageBackground}
+      >
         <View style={styles.contentContainer}>
-          <Image 
-            source={require('../assets/favicon.png')} 
+          <Image
+            source={require("sbhacks2024/assets/logo4.png")}
             style={styles.image}
           />
-          <Text style={styles.titleText}>Keep all your client conversations in one place</Text>
-          <Text style={styles.baseText}>At legal call we allow you to contact your clients through voice and text without giving out your phone number</Text>
-          <Pressable style={styles.button} onPress={() => navigation.navigate('Sign In')}>
-            <Text style={styles.buttonText}>Sign In</Text>
-          </Pressable>
-          <Pressable style={styles.button} onPress={() => navigation.navigate('Sign Up')}>
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </Pressable>
+          <Text style={styles.titleText}>Memori</Text>
+          <Text style={styles.baseText}>
+            Preserving memories for generations to come
+          </Text>
+
+          <View style={styles.buttonContainer}>
+            <Pressable
+              style={styles.button}
+              onPress={() => navigation.navigate("Sign In")}
+            >
+              <Text style={styles.buttonText}>Sign In</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button2}
+              onPress={() => navigation.navigate("Sign Up")}
+            >
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </Pressable>
+          </View>
         </View>
-      </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
@@ -29,48 +50,60 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  linearGradient: {
+  imageBackground: {
     flex: 1,
-    borderRadius: 20
+    borderRadius: 20,
   },
   contentContainer: {
     margin: 16,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // Adjust the gap between elements as needed
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
-    width: 250,
-    height: 250,
-    alignSelf: 'center'
+    width: 180,
+    height: 180,
+    alignSelf: "center",
   },
   titleText: {
-    color: 'blue', // Replace with your desired color
-    fontSize: 24, // Equivalent to 2xl
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginHorizontal: 24,
+    fontSize: 60,
+    textAlign: "center",
+    fontFamily: "marcellus",
+    color: "#261e29",
   },
   baseText: {
-    color: 'white', // Replace with your desired color
-    fontSize: 16, // Equivalent to base
-    textAlign: 'center',
+    color: "black",
+    fontSize: 18,
+    textAlign: "center",
     marginHorizontal: 16,
+    fontFamily: "marcellus",
+    paddingBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row", // Aligns buttons in a row
+    justifyContent: "center", // Centers buttons horizontally
   },
   button: {
-    backgroundColor: 'blue', // Replace with your desired color
-    borderRadius: 30, // Equivalent to rounded-3xl
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    margin: 16,
+    backgroundColor: "#6c636b",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 24,
+    margin: 5, // Add some margin to separate the buttons
+  },
+  button2: {
+    backgroundColor: "#6c636b",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    margin: 5, // Add some margin to separate the buttons
   },
   buttonText: {
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 16,
-  }
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+    fontFamily: "marcellus",
+  },
 });
-    
+
 export default WelcomeScreen;
