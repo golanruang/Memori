1f92fb093d9880bbccbb46f17dbda5df9912057a
Lindsey Wen <lindseywen@ucsb.edu>
2024-01-13 23:05:21 -0800

    updated frontend

--- sbhacks2024/bottomMenu.jsx
+++ sbhacks2024/bottomMenu.jsx
@@ -4,15 +4,88 @@ import { Text } from 'react-native';
 import HomeScreen from './screens/home';
 import SelectTopicScreen from './screens/select_topic';
 import Settings from './screens/Settings';
+import { Image } from 'react-native'; 
+import { StyleSheet } from 'react-native';
+
 
 const Tab = createBottomTabNavigator();
 
-export default function BottomMenu() {
+const BottomMenu = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} options = {{headerShown: false}}/>
-            <Tab.Screen name="SelectTopic" component={SelectTopicScreen} options = {{headerShown: false}}/>
-            <Tab.Screen name="Settings" component={Settings} options = {{headerShown: false}}/>
-        </Tab.Navigator>   
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false, 
+          style: {
+            height: 60,
+          },
+          tabStyle: {
+            paddingTop: 5, 
+            paddingBottom: 5,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("sbhacks2024/assets/home2.png")
+                    : require("sbhacks2024/assets/home1.png")
+                }
+                style={{ width: 45, height: 45 }}
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="New Entry"
+          component={SelectTopicScreen}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("sbhacks2024/assets/add2.png")
+                    : require("sbhacks2024/assets/add1.png")
+                }
+                style={{ width: 45, height: 45 }}
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("sbhacks2024/assets/profile2.png")
+                    : require("sbhacks2024/assets/profile1.png")
+                }
+                style={{ width: 60, height: 60 }}
+              />
+            ),
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
     );
   }
+
+  const styles = StyleSheet.create({
+    name: {
+        fontFamily: 'marcellus',
+    },
+});
+
+export default BottomMenu;
